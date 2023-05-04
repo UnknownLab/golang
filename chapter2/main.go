2098ea6135f7f7e0579572f1b025e28d6e9d8892
UnknownHero <unknownpostie@gmail.com>
2023-05-05 02:36:11 +0400

    feat: Add first golang script
    This commit adds file for first golang program from book

---
+++ chapter2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// this is a comment
+/*
+this is a comment too
+*/
+func main() {
+	fmt.Println("Hello, my name is UnknownHero")
+	//（っ＾▿＾）
+	os.Exit(1)
+}
